42cecff0cb4093f87ef62bc4f39d9ec295a6e78c
gallenfelicen <gallen.leslei.felicen@eee.upd.edu.ph>
2023-11-30 03:30:24 +0800

    Update receive.js

--- services/receive.js
+++ services/receive.js
@@ -103,8 +103,8 @@ module.exports = class Receive {
   async generateGptResponse(message, previousMessages = []) {
     try {
       // Make an API call to OpenAI GPT
-      const previousMessage = await this.getMessages(this.user.psid);
-      console.log("previousMessages: ", previousMessage, "PSID: ", this.user.psid);
+      // const previousMessages = await this.getMessages(this.user.psid);
+      console.log("previousMessages: ", previousMessages, "PSID: ",this.user.psid);
 
       const response = await openai.chat.completions.create(
         {
